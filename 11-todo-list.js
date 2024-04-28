661dc3ee47204f81bfc561738d9ada3ec8f9ef26
pratiyankkumar <kumarpratiyank@gmail.com>
2024-04-28 11:35:57 +0530

    added .forEach() instead of using for loop

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -14,21 +14,20 @@ renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
-  for (let i=0; i<todoList.length; i++) {
-    
-    const todoObject = todoList[i];
+
+  todoList.forEach(function(todoObject, index) {
     const {name,dueDate} = todoObject;
 
     const html = `
       <div>${name}</div>
       <div>${dueDate}</div>
       <button onclick="
-      todoList.splice(${i},1);
+      todoList.splice(${index},1);
       renderTodoList();
       " class="delete-todo-button">Delete</button>
     `;
     todoListHTML+=html
-  }
+  });
 
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
