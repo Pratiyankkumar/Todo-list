d46e05d1568d56f01c72c58dc3d4b698ac182f91
pratiyankkumar <kumarpratiyank@gmail.com>
2024-04-28 09:10:05 +0530

    Updated

---
+++ 11-todo-list.js
@@ -0,0 +1,56 @@
+
+
+let todoList = [];
+
+// Check if there are any existing todoList items stored in localStorage
+const storedTodoList = localStorage.getItem('todoList');
+if (storedTodoList) {
+  todoList = JSON.parse(storedTodoList);
+}
+
+// Call renderTodoList function to initially render the todo list
+renderTodoList();
+
+
+function renderTodoList() {
+  let todoListHTML = '';
+  for (let i=0; i<todoList.length; i++) {
+    
+    const todoObject = todoList[i];
+    const {name,dueDate} = todoObject;
+
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+      todoList.splice(${i},1);
+      renderTodoList();
+      " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML+=html
+  }
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+}
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+  
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
+
+  updateLocalStorage();
+  renderTodoList();
+  
+}
+function updateLocalStorage() {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
